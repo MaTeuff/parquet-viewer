15da10e285341f266b56f25a8939a0a6b12d1d4f
Maximilian Teuffenbach <teuffenbach@gmail.com>
2024-12-06 11:22:47 +0100

    Remove all logos and stuff of vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { useReactTable, getCoreRowModel, flexRender } from '@tanstack/react-table'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   type Car = {
     manufacturer: string
     color: string
@@ -89,23 +85,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Parquet Viewer and Editor</h1>
       <div>
         <table>
           <thead>
@@ -137,9 +117,6 @@ function App() {
           </tbody>
         </table>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
